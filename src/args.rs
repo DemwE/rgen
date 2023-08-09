b5343f6c8918d1575dd7775b5f9da7889ebbe214
DemwE <demweee@gmail.com>
2023-08-09 14:03:48 +0200

    args

---
+++ src/args.rs
@@ -0,0 +1,23 @@
+use clap::{command, Parser};
+
+#[derive(Debug, Default, Parser)]
+#[command(name = env!("CARGO_PKG_NAME"))]
+#[command(author = env!("CARGO_PKG_AUTHORS"))]
+#[command(version = env!("CARGO_PKG_VERSION"))]
+#[command(about = env!("CARGO_PKG_DESCRIPTION"))]
+#[command(
+help_template = "{name} {version} {author-section} {about-with-newline} \n {all-args}"
+)]
+pub struct RgenArgs {
+    /// Password length
+    #[clap(default_value = "16")]
+    #[clap(short, long)]
+    pub length: Option<usize>,
+    /// Password count
+    #[clap(default_value = "1")]
+    #[clap(short, long)]
+    pub count: Option<usize>,
+    /// Use special characters
+    #[clap(short, long)]
+    pub special: Option<bool>,
+}
